b2e16aec7a73e7b3662bcbc3b4c177e47badc537
qiezhengyuan <qiezhengyuan@wiyit.com>
2024-10-23 13:59:54 +0800

    修改部分组件

--- src/input/input.js
+++ src/input/input.js
@@ -3,13 +3,19 @@ export default {
     style: import('./input.scss'),
     data: {
         text: '',
+        showPassword: false,
     },
     methods: {
         onInputChange() {
             this.trigger('change', {
                 text: this.text,
             });
-        }
+        },
+        onPasswordToggleClick() {
+            const input = this.getElement('input');
+            input.type = input.type === 'password' ? 'text' : 'password';
+            this.showPassword = !this.showPassword;
+        },
     },
     lifecycle: {
         init() {
