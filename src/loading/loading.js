8fb95b898aa9d30e02687ea55a5bf01d0ca24651
qiezhengyuan <qiezhengyuan@wiyit.com>
2024-11-11 18:04:53 +0800

    修改部分组件

--- src/loading/loading.js
+++ src/loading/loading.js
@@ -1,4 +1,25 @@
 export default {
     template: import('./loading.html'),
     style: import('./loading.scss'),
+    data: {
+        loading: false,
+    },
+    lifecycle: {
+        mount() {
+            const mask = this.getElement('mask');
+            this.observe(() => {
+                return this.loading;
+            }, (result) => {
+                if (result) {
+                    if (this.hasAttr('fullscreen')) {
+                        mask.showModal();
+                    } else {
+                        mask.show();
+                    }
+                } else {
+                    mask.close();
+                }
+            });
+        },
+    },
 };
