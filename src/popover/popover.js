ecd011a2b03de787f68ad6d69631b0fceb8d83fd
qiezhengyuan <qiezhengyuan@wiyit.com>
2024-11-13 14:29:44 +0800

    修改部分组件

--- src/popover/popover.js
+++ src/popover/popover.js
@@ -14,15 +14,29 @@ export default {
     },
     lifecycle: {
         mount() {
-            const popover = this.getElement('popover');
-            if (popover) {
+            if (this.attr('popover-pos') == 'mouse') {//当根据鼠标位置定位时，通过popover属性及showPopover、hidePopover方法来显示、隐藏弹出框
                 const source = this.getElement('source');
-                if (this.attr('popover-pos') == 'mouse') {
+                const popoverContainer = this.getElement('popover-container');
+                const popover = this.getElement('popover');
+
+                if (popoverContainer) {
+                    popoverContainer.setAttribute('popover', 'manual');//设置popover为manual，不允许点击关闭
+
                     source.addEventListener('mousemove', (e) => {
                         const x = e.clientX + 16;
                         const y = e.clientY + 16;
                         popover.style.left = x + 'px';
                         popover.style.top = y + 'px';
+                    });
+
+                    this.observe(() => {
+                        return this.visible;
+                    }, (result) => {
+                        if (result) {
+                            popoverContainer.showPopover();
+                        } else {
+                            popoverContainer.hidePopover();
+                        }
                     });
                 }
             }
