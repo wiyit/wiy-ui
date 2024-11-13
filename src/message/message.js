2c0e4b4b7f6dc63a8f40b15123d91004539428b9
qiezhengyuan <qiezhengyuan@wiyit.com>
2024-11-13 20:36:23 +0800

    修改部分组件

--- src/message/message.js
+++ src/message/message.js
@@ -1,6 +1,25 @@
+function getLayer() {
+    const layerTagName = 'wiy-message-layer';
+    let layer = document.getElementsByTagName(layerTagName)[0];
+    if (layer) {
+        return layer;
+    }
+
+    layer = document.createElement(layerTagName);
+    layer.setAttribute('popover', 'manual');
+    layer.style = 'margin:10px auto;padding:0;border:none;overflow:visible;background:none;display:flex;flex-direction:column;align-items:center;';
+    document.body.appendChild(layer);
+
+    layer.showPopover();
+    return layer;
+}
+
 export default {
     template: import('./message.html'),
     style: import('./message.scss'),
+    data: {
+        visible: true,
+    },
     methods: {
         getIcon() {
             switch (this.attr('theme') || 'info') {
@@ -18,6 +37,22 @@ export default {
                     return 'spinner';
                 default:
                     return 'circle-info';
+            }
+        },
+    },
+    lifecycle: {
+        mount() {
+            const position = this.attr('position') || 'static';
+            if (position != 'static') {
+                const layer = getLayer(position);
+                layer.appendChild(this._element);
+            }
+
+            const duration = this.attr('duration') || 3000;
+            if (duration >= 0) {
+                setTimeout(() => {
+                    this.visible = false;
+                }, duration);
             }
         },
     },
