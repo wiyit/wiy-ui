1c008875bcca640c384b2bb0e4e0ba428292bcc3
qiezhengyuan <qiezhengyuan@wiyit.com>
2024-12-05 17:45:11 +0800

    修改部分组件

--- src/chart/chart.js
+++ src/chart/chart.js
@@ -11,28 +11,50 @@ export default {
         mount() {
             const container = this.getElement('container');
 
+            const update = () => {
+                const width = container.clientWidth;
+                const height = container.clientHeight;
+
+                if (this.chart) {//有图表
+                    if (width != this.chart.getWidth() || height != this.chart.getHeight()) {//图表大小改变，则重置大小
+                        this.chart.resize();
+                        return;
+                    }
+                } else {//没有图表
+                    if (width > 0 && height > 0) {//图表变为可见，则初始化图表
+                        Object.defineProperty(this, 'chart', {
+                            writable: false,
+                            value: echarts.init(container, this.attr('theme')),
+                        });
+
+                        this.chart.on('rendered', () => {
+                            this.trigger('rendered');
+                        });
+                        this.chart.on('selectchanged', (e) => {
+                            this.trigger('selectchanged', {
+                                selected: e.selected,
+                            }, e);
+                        });
+
+                        this.observe(() => {
+                            return this.option;
+                        }, (result) => {
+                            this.chart.setOption(result);
+                        });
+                    }
+                }
+            };
+
             new ResizeObserver(() => {
-                if (this.chart) {
-                    this.chart.resize();
-                    return;
-                }
-
-                if (container.clientWidth && container.clientHeight) {
-                    Object.defineProperty(this, 'chart', {
-                        writable: false,
-                        value: echarts.init(container, this.attr('theme')),
-                    });
-
-                    this.observe(() => {
-                        return this.option;
-                    }, (result) => {
-                        this.chart.setOption(result);
-                    });
-                }
+                requestAnimationFrame(update);
             }).observe(container);
+        },
+        unmount() {
+            this.chart && this.chart.dispose();
         },
     },
 };
+
 export const methods = {
     registerChartTheme(name, theme) {
         echarts.registerTheme(name, theme);
