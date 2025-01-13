01b3df6ec7423331c5fb63e8c1225c4d41eb3b84
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-01-13 19:31:18 +0800

    修改部分组件

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -1,7 +1,32 @@
 export default {
     template: import('./image.html'),
-    style: import('./image.css'),
+    style: import('./image.scss'),
     data: {
         src: undefined,
+        srcList: [],
+        current: undefined,
+    },
+    methods: {
+        getCurrentPreviewSrc() {
+            this.current = Math.min(Math.max(this.current || this.srcList.indexOf(this.src), 0), this.srcList.length - 1);
+            return this.srcList[this.current];
+        },
+        closePreview(e) {
+            e && e.stopPropagation();
+            const preview = this.getElement('preview');
+            preview && preview.close();
+        },
+    },
+    lifecycle: {
+        mount() {
+            const element = this.getElement();
+            element.addEventListener('click', () => {
+                const preview = this.getElement('preview');
+                preview && preview.showModal();
+            });
+        },
+        beforeUnmount() {
+            this.closePreview();
+        },
     },
 };
