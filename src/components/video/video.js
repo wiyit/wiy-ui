bb09b655040e4707d46ee04e7fdf648ee5a11e2a
makefire <1710401242@qq.com>
2025-01-14 17:23:19 +0800

    Reapply "修改组件：video"
    This reverts commit 578de0ee198f21e7df53cab2f2ecaabc30e68137.

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -4,4 +4,13 @@ export default {
     data: {
         src: undefined,
     },
+    lifecycle: {
+        mount() {
+            const video = this.getElement('video');
+            console.log(video)
+            video.addEventListener('ended', () => {
+                this.trigger('ended');
+            });
+        },
+    },
 };
