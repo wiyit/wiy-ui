7b76b64dbdabc6d5e737925b6277bf5e5d12d66d
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-02-19 04:00:16 +0800

    修改部分组件

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -3,12 +3,27 @@ export default {
     style: import('./video.scss'),
     data: {
         src: undefined,
+        poster: undefined,
+    },
+    methods: {
+        oncontextmenu(e) {
+            e.preventDefault();
+            return false;
+        },
     },
     lifecycle: {
         mount() {
             if (this.hasAttr('autoplay')) {
                 const video = this.getElement('video');
                 video.muted = true;
+
+                this.observe(() => {
+                    return this.src;
+                }, (result) => {
+                    setTimeout(() => {
+                        video.play();
+                    }, this.attr('autoplay-delay') || 0);
+                });
             }
         },
     },
