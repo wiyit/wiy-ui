504138033cc224bb97b07283736d032147dd38a1
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-02-20 23:06:54 +0800

    修改部分组件

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -10,18 +10,36 @@ export default {
             e.preventDefault();
             return false;
         },
+        setTime(time) {
+            const video = this.getElement('video');
+            video.currentTime = time;
+        },
+        play(time) {
+            const video = this.getElement('video');
+            if (typeof time != 'undefined') {
+                this.setTime(time);
+            }
+            video.play().catch((e) => {
+                video.muted = true;
+                video.play();
+            });
+        },
+        pause(time) {
+            const video = this.getElement('video');
+            if (typeof time != 'undefined') {
+                this.setTime(time);
+            }
+            video.pause();
+        },
     },
     lifecycle: {
         mount() {
             if (this.hasAttr('autoplay')) {
-                const video = this.getElement('video');
-                video.muted = true;
-
                 this.observe(() => {
                     return this.src;
                 }, (result) => {
                     setTimeout(() => {
-                        video.play();
+                        this.play();
                     }, this.attr('autoplay-delay') || 0);
                 });
             }
