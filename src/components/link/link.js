d6496676b589ebaf4f98072555da889acfc45439
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-03-24 16:26:44 +0800

    修改部分组件

--- src/components/link/link.js
+++ src/components/link/link.js
@@ -1,4 +1,16 @@
 export default {
     template: import('./link.html'),
     style: import('./link.scss'),
+    methods: {
+        onClick(e) {
+            const href = this.attr('href');
+            if (href) {
+                const router = this.getApp().getRouter();
+                if (router.isInternalLink(href)) {//在路由范围内
+                    e.preventDefault();
+                    router.go(router.toRelativePath(href), undefined, false);
+                }
+            }
+        },
+    },
 };
