81bafa12d34d6fcc28b695cc637f267d2586bda5
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-03-24 16:49:02 +0800

    修改部分组件

--- src/components/link/link.js
+++ src/components/link/link.js
@@ -3,13 +3,19 @@ export default {
     style: import('./link.scss'),
     methods: {
         onClick(e) {
-            const href = this.attr('href');
-            if (href) {
-                const router = this.getApp().getRouter();
-                if (router.isInternalLink(href)) {//在路由范围内
-                    e.preventDefault();
-                    router.go(router.toRelativePath(href), undefined, false);
-                }
+            const href = this.attr('href') || '';
+            const target = this.attr('target') || '_self';
+            if (href.startsWith('http://') || href.startsWith('https://')) {
+                return;
+            }
+            if (target != '_self') {
+                return;
+            }
+
+            const router = this.getApp().getRouter();
+            if (router.isInternalLink(href)) {//在路由范围内
+                e.preventDefault();
+                router.go(router.toRelativePath(href), undefined, false);
             }
         },
     },
