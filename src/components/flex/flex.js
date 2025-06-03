9b3c38c71353912462cf99c55b12cffefb80b0f3
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-06-03 11:54:18 +0800

    1. flex组件支持gap属性 2. image、video组件调整为默认大小

--- src/components/flex/flex.js
+++ src/components/flex/flex.js
@@ -1,4 +1,10 @@
 export default {
     template: import('./flex.html'),
     style: import('./flex.scss'),
+    lifecycle: {
+        mount() {
+            const element = this.getElement();
+            element.style.gap = this.attr('gap') || '0';
+        }
+    }
 };
