93c031e36d959184b6813d0e2dc4108594f7e91c
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-06-07 18:59:37 +0800

    1. 修改checkbox-group数据格式及逻辑 2. checkbox-group中的options支持定义为函数

--- src/components/checkbox-group/checkbox-group.js
+++ src/components/checkbox-group/checkbox-group.js
@@ -1,13 +1,36 @@
 export default {
     template: import('./checkbox-group.html'),
-    style: import('./checkbox-group.css'),
+    style: import('./checkbox-group.scss'),
     data: {
         options: [],
-        data: {},
+        data: [],
     },
     methods: {
-        onInputChange() {
-            this.trigger('change', {
+        getOptions() {
+            const options = this.options;
+            if (typeof options == 'function') {
+                return options();
+            }
+            return options;
+        },
+        onInputChange(e, value) {
+            const data = this.data;
+            const { checked } = e.target;
+            let changed = false;
+            if (checked) {
+                if (!data.includes(value)) {
+                    data.push(value);
+                    changed = true;
+                }
+            } else {
+                let index;
+                while ((index = data.indexOf(value)) >= 0) {
+                    data.splice(index, 1);
+                    changed = true;
+                }
+            }
+
+            changed && this.trigger('change', {
             });
         },
     },
