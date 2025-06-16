25f67d2a87ff85c1cdb02279bb10c967a657c0a4
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-06-16 17:20:37 +0800

    form中的items、operations、data支持定义为函数

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -9,6 +9,18 @@ export default {
         data: {},
     },
     methods: {
+        async getItems() {
+            this.actualItems = await this.actual(this.items);
+            return this.actualItems;//返回响应式结果
+        },
+        async getOperations() {
+            this.actualOperations = await this.actual(this.operations);
+            return this.actualOperations;//返回响应式结果
+        },
+        async getData() {
+            this.actualData = await this.actual(this.data);
+            return this.actualData;//返回响应式结果
+        },
         onOperationClick(operation) {
             operation.onclick && operation.onclick(this, operation);
         },
@@ -17,6 +29,10 @@ export default {
         },
         onItemValueChange(item) {
             item.onchange && item.onchange(this, item);
+        },
+        onItemDestroy(item) {
+            delete this.actualData[item.name];
+            item.ondestroy && item.ondestroy(this, item);
         },
         needShowItem(item) {
             return !item.when || item.when(this, item);
