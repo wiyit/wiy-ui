d9fcfe87f5cd9f784591dff63cb6a71ddbee7b44
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-06-17 23:47:50 +0800

    radio-group支持输入其他值

--- src/components/radio-group/radio-group.js
+++ src/components/radio-group/radio-group.js
@@ -10,6 +10,9 @@ export default {
             this.actualOptions = await this.actual(this.options);
             return this.actualOptions;//返回响应式结果
         },
+        isOther() {
+            return this.data && !this.actualOptions.some(o => this.data === o.value);
+        },
         onInputChange(e, value) {
             this.trigger('change', {
                 data: this.data = value,
