2e3e37112b1f2cee795a89837a749298d7f506fd
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-07-02 19:49:24 +0800

    fixbug

--- src/components/radio-group/radio-group.js
+++ src/components/radio-group/radio-group.js
@@ -10,14 +10,21 @@ export default {
             this.actualOptions = await this.actual(this.options);
             return this.actualOptions;//返回响应式结果
         },
-        isOther() {
-            return this.data && !this.actualOptions.some(o => this.data === o.value);
+        getOtherData() {
+            if (!this.actualOptions.some(o => this.data === o.value)) {
+                return this.data;
+            }
         },
         onInputChange(e, value) {
             this.trigger('change', {
                 data: this.data = value,
             });
         },
+        onOtherChange(e, otherData) {
+            this.trigger('change', {
+                data: this.data = otherData,
+            });
+        }
     },
     lifecycle: {
         init() {
