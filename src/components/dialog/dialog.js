0e81e51277f093246af9fd790838ee834ca9ef25
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-07-03 20:33:06 +0800

    1. dialog支持在关闭时触发close事件 2. 将showDialog注册为全局方法，可通过app实例的showDialog方法打开一个dialog

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -27,3 +27,30 @@ export default {
         },
     },
 };
+export const methods = {
+    showDialog(options) {
+        const element = document.createElement('wiy-dialog');
+        element.id = 'dialog';
+        options.title && element.setAttribute('title', options.title);
+        element.setAttribute('wiy:onmount', "show");
+        element.setAttribute('wiy:onclose', "remove");
+        element.innerHTML = options.content;
+        document.body.appendChild(element);
+
+        const component = this.newComponent({
+            components: {
+                'wiy-dialog': import('./dialog.js'),
+            },
+        });
+        component.renderElement(element, [
+            {
+                show() {
+                    component.getComponent('dialog').showModal();
+                },
+                remove() {
+                    component.getComponent('dialog').remove();
+                }
+            }
+        ]);
+    },
+};
