a192881e53edd90b1a80d0b6b2b37801e17fda0b
qiezhengyuan <qiezhengyuan@wiyit.com>
2025-11-17 13:50:24 +0800

    1. carousel支持双向数据绑定 2. carousel支持通过smooth属性设置滚动行为 3. carousel中的图片支持通过blurBackground属性设置模糊背景

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -11,27 +11,16 @@ export default {
     methods: {
         goPrev() {
             const count = this.items.length;
-            this.setActive((this.active - 1 + count) % count);
+            this.active = (this.active - 1 + count) % count;
         },
         goNext() {
             const count = this.items.length;
-            this.setActive((this.active + 1 + count) % count);
-        },
-        setActive(index) {
-            const items = this.getElement('items');
-            const item = this.getElement(`item-${index}`);
-            if (item) {
-                items.scrollTo({
-                    left: item.offsetLeft,
-                    behavior: 'smooth',
-                });
-                this.active = index;
-            }
+            this.active = (this.active + 1 + count) % count;
         },
         play(index) {
             this.pause();
             if (typeof index != 'undefined') {
-                this.setActive(index);
+                this.active = index;
             }
             this.autoplayIntervalId = setInterval(() => {
                 this.goNext();
@@ -39,7 +28,7 @@ export default {
         },
         pause(index) {
             if (typeof index != 'undefined') {
-                this.setActive(index);
+                this.active = index;
             }
             clearInterval(this.autoplayIntervalId);
         },
@@ -68,6 +57,25 @@ export default {
         },
     },
     lifecycle: {
+        init() {
+            this.observe(() => {
+                return this.active;
+            }, ({ result, firstObserve }) => {
+                this.active = Math.max(Math.min(result, this.items.length - 1), 0);
+
+                const items = this.getElement('items');
+                const item = this.getElement(`item-${this.active}`);
+                if (item) {
+                    items.scrollTo({
+                        left: item.offsetLeft,
+                    });
+                }
+
+                this.trigger(firstObserve ? 'datainit' : 'change', {
+                    active: this.active,
+                });
+            });
+        },
         mount() {
             if (this.hasAttr('autoplay')) {
                 this.autoplay();
